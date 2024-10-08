2c1cb0620f115c6874d4de112104fe1b232f7b32
Yash1943 <joshiyash1942003@gmail.com>
2024-10-08 22:17:10 +0530

    AllClassComponetns_converted_Function

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,24 +1,29 @@
 import "./TaskCard.css";
-import React from "react";
 import { TaskItem } from "./types";
 // ...
 
 interface TaskProps extends TaskItem {}
 
-class Task extends React.Component<TaskProps> {
-  render() {
-    return (
-      <div className="TaskItem shadow-md border border-slate-100">
-        <h2>{this.props.title}</h2>
-        {this.props.dueDate && (
-          <p>Due Date: {this.props.dueDate}</p>
-        )}
-        {this.props.description && (
-          <p>Description: {this.props.description}</p>
-        )}
-      </div>
-    );
-  }
-}
+// class Task extends React.Component<TaskProps> {
+//   render() {
+//     return (
+//       <div className="TaskItem shadow-md border border-slate-100">
+//         <h2>{this.props.title}</h2>
+//         {this.props.dueDate && <p>Due Date: {this.props.dueDate}</p>}
+//         {this.props.description && <p>Description: {this.props.description}</p>}
+//       </div>
+//     );
+//   }
+// }
+
+const Task = (props: TaskProps) => {
+  return (
+    <div className="TaskItem shadow-md border border-slate-100">
+      <h2>{props.title}</h2>
+      {props.dueDate && <p>Due Date: {props.dueDate}</p>}
+      {props.description && <p>Description: {props.description}</p>}
+    </div>
+  );
+};
 
 export default Task;
--- smarter-tasks/src/TaskList.tsx
+++ smarter-tasks/src/TaskList.tsx
@@ -1,17 +1,23 @@
-import React from "react";
 import Task from "./Task";
 import { TaskItem } from "./types";
 
 interface Props {
   tasks: TaskItem[];
 }
-class TaskList extends React.Component<Props> {
-  render() {
-    return this.props.tasks.map((task, idx) => (
-      <div key={idx}>
-        <Task key={idx} title={task.title} dueDate={task.dueDate} description={task.description} />
-      </div>
-    ));
-  }
-}
+// class TaskList extends React.Component<Props> {
+//   render() {
+//     return this.props.tasks.map((task, idx) => (
+//       <div key={idx}>
+//         <Task key={idx} title={task.title} dueDate={task.dueDate} description={task.description} />
+//       </div>
+//     ));
+//   }
+// }
+
+const TaskList = (props: Props) => {
+  const list = props.tasks.map((task, idx) => (
+    <Task key={idx} title={task.title} dueDate={task.dueDate} description={task.description} />
+  ));
+  return <>{list}</>;
+};
 export default TaskList;
