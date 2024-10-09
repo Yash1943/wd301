1c2881e6c2e51c3d0802ccb261a09cb26d424ec7
Yash1943 <joshiyash1942003@gmail.com>
2024-10-09 20:50:42 +0530

    HomeRoute_fix

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import TaskApp from "./TaskApp";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import TaskListPage from "./pages/TaskListPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/tasks",
+    element: <TaskListPage />,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <TaskApp />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 export default App;
