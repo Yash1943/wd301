67a2f6e8368c60c7434077912d39671b7e4e1ce9
Yash1943 <joshiyash1942003@gmail.com>
2024-10-10 10:40:44 +0530

    notFound_src_fix

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -7,7 +7,7 @@ import TaskDetailsPage from "./pages/TaskDetailsPage";
 import Layout from "./Layout";
 import Signin from "./pages/Signin";
 import ProtectedRoute from "./ProtectedRoute";
-import NotFoundPage from "./pages/NotFoundPage";
+import NotFoundPage from "./pages/Notfound";
 
 const router = createBrowserRouter([
   {
